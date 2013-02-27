1e85a76b8b8c53c88ccc1dae5d7f7212fe53a2c9
Tom Ansley <tomansley@gmail.com>
2013-02-27 09:23:21 -0600

    Adding json util class

--- src/com/gdelight/tools/json/JsonUtils.java
+++ src/com/gdelight/tools/json/JsonUtils.java
@@ -1,5 +1,18 @@
 package com.gdelight.tools.json;
 
+import com.google.gson.Gson;
+
 public class JsonUtils {
 
+	public static Object parseJSonDocument(String json, Class clazz) {
+		
+		Object n = null;
+		
+		Gson gson = new Gson();
+		
+		Object obj = gson.fromJson(json, clazz);
+		
+		return n;
+	}
+	
 }

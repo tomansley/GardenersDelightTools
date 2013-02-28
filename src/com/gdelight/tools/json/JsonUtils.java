dee9d24c89484f8b2a2dbf0c4266b3f49cb06de4
Tom Ansley <tomansley@gmail.com>
2013-02-28 11:13:01 -0600

    Adding json utils.

--- src/com/gdelight/tools/json/JsonUtils.java
+++ src/com/gdelight/tools/json/JsonUtils.java
@@ -5,14 +5,18 @@ import com.google.gson.Gson;
 public class JsonUtils {
 
 	public static Object parseJSonDocument(String json, Class clazz) {
-		
-		Object n = null;
-		
+
 		Gson gson = new Gson();
 		
 		Object obj = gson.fromJson(json, clazz);
 		
-		return n;
+		return obj;
+	}
+	
+	public static String getJSonDocument(Object obj) {
+		Gson gson = new Gson();
+		String json = gson.toJson(obj);
+		return json;
 	}
 	
 }
